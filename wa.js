7d94389980c432318ed5251f30ec3aa96ada4fcc
fxsth <fxsth@pm.me>
2020-04-17 21:29:49 +0200

    test files

---
+++ wa.js
@@ -0,0 +1,14 @@
+let add;
+
+function loadWasm(filename)
+{
+    return fetch(filename)
+    .then(response => response.arrayBuffer())
+    .then(bits => WebAssembly.compile(bits))
+    .then(module => {return new WebAssembly.Instance(module) });
+};
+
+loadWasm('test.wasm')
+.then(instance => {
+    add = instance.export._Z3addii;
+})
